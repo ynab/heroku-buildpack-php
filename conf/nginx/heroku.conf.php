10239ff0c9455fc1f220db7127bf0ba84c86e03b
Brady Holt <brady.holt@gmail.com>
2017-08-11 13:08:44 -0500

    If .php script doesn't exists redirect to /

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -31,32 +31,32 @@ http {
         # define an easy to reference name that can be used in try_files
         location @heroku-fcgi {
             include fastcgi_params;
-            
+
             fastcgi_split_path_info ^(.+\.php)(/.*)$;
             fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
             # try_files resets $fastcgi_path_info, see http://trac.nginx.org/nginx/ticket/321, so we use the if instead
             fastcgi_param PATH_INFO $fastcgi_path_info if_not_empty;
-            
+
             if (!-f $document_root$fastcgi_script_name) {
                 # check if the script exists
                 # otherwise, /foo.jpg/bar.php would get passed to FPM, which wouldn't run it as it's not in the list of allowed extensions, but this check is a good idea anyway, just in case
-                return 404;
+                return 301 /;
             }
-            
+
             fastcgi_pass heroku-fcgi;
         }
-        
+
         # TODO: use X-Forwarded-Host? http://comments.gmane.org/gmane.comp.web.nginx.english/2170
         server_name localhost;
         listen <?=getenv('PORT')?:'8080'?>;
         # FIXME: breaks redirects with foreman
         port_in_redirect off;
-        
+
         root "<?=getenv('DOCUMENT_ROOT')?:getenv('HEROKU_APP_DIR')?:getcwd()?>";
-        
+
         error_log stderr;
         access_log /tmp/heroku.nginx_access.<?=getenv('PORT')?:'8080'?>.log;
-        
+
         # Force TLS
         if ($http_x_forwarded_proto != "https") {
             return 301 https://$host$request_uri;
@@ -67,19 +67,19 @@ http {
             auth_basic_user_file <?=getenv('HEROKU_APP_DIR')?:getcwd()?>/.htpasswd;
             try_files @heroku-fcgi @heroku-fcgi;
         }
-        
+
         location ~* /wp-admin/(.+)$ {
             auth_basic "Login";
             auth_basic_user_file <?=getenv('HEROKU_APP_DIR')?:getcwd()?>/.htpasswd;
 
             add_header Content-Security-Policy "default-src 'self' * 'unsafe-inline' 'unsafe-eval' data:";
- 			   
+
             # Pass to fastcgi upstream directly to preserve add_header
             include fastcgi_params;
             fastcgi_split_path_info ^(.+\.php)(/.*)$;
             fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
             fastcgi_param PATH_INFO $fastcgi_path_info if_not_empty;
-            fastcgi_pass heroku-fcgi;         
+            fastcgi_pass heroku-fcgi;
         }
 
         location = /wp-admin/admin-ajax.php {
@@ -88,12 +88,12 @@ http {
         }
 
         include "<?=getenv('HEROKU_PHP_NGINX_CONFIG_INCLUDE')?>";
-        
+
         # restrict access to hidden files, just in case
         location ~ /\. {
             deny all;
         }
-        
+
         # default handling of .php
         location ~ \.php {
             try_files @heroku-fcgi @heroku-fcgi;
